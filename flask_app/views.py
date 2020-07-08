3a70a1f5bb287b234ed2d6c1a4dfae54596a11f6
slashformotion <slashformotion@protonmail.com>
2020-07-08 22:24:47 +0200

    test don't push already

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,9 +1,21 @@
 from flask_app import app
 from flask import render_template, url_for, make_response, request, redirect, session
 
-
+@app.errorhandler(404)
+def error_handler_404(error):
+    # print(error)
+    return render_template("public/404.html", error=error)
 
 @app.route("/")
 def index():
     return render_template("public/index.html")
 
+
+@app.route("/token")
+def token():
+    return render_template('public/nada.html', page_name="token")
+
+
+@app.route("/user")
+def user():
+    return render_template('public/nada.html', page_name="user")
---
+++ run.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=worker
+export FLASK_ENV=development
+flask run 
