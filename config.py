418b6a7b8a0dcf49aa15e3a9c08b28fa19083b58
slashformotion <slashformotion@protonmail.com>
2020-07-09 18:44:49 +0200

    add: config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import environ
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    
+
+    SESSION_COOKIE_SECURE = True
+
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class TestingConfig(Config):
+    TESTING = True
+
+    SESSION_COOKIE_SECURE = False
--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+if app.config.get("ENV") == "production":
+    app.config.from_object("config.ProductionConfig")
+elif app.config.get("ENV") == "testing":
+    app.config.from_object("config.TestingConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
 
 from flask_app import views
